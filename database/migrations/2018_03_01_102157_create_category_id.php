42d68fa2be8854b8c2436d57eb8ed8e40b11ff4f
aiman kabbani <aimankabbani@gmail.com>
2018-03-23 04:15:37 +0200

    change datatype category timestamp

--- database/migrations/2018_03_01_102157_create_category_id.php
+++ database/migrations/2018_03_01_102157_create_category_id.php
@@ -20,7 +20,7 @@ class CreateCategoryId extends Migration
             $table->string('kurdi');
             $table->string('turky');
             $table->string('german');
-            $table->timestamps();
+            //$table->timestamps();
             });
     }
 
