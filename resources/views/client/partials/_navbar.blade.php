0794585da35b1bf247bff185cc2a120ff61cc8ba
aiman kabbani <aimankabbani@gmail.com>
2018-04-30 12:12:04 +0300

    add lan flag

--- resources/views/client/partials/_navbar.blade.php
+++ resources/views/client/partials/_navbar.blade.php
@@ -6,36 +6,36 @@
             <li class="nav-item active">
               <a class="nav-link" href="{{ route('home') }}">@lang('Home')</a>
             </li>
-         
+
             @if(!Auth::check() )
-          
+
             <li class="nav-item">
               <a class="nav-link" href="{{route('login')}}">@lang('Login')</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="{{route('register')}}">@lang('Sign up')</a>
             </li>
-            
+
             @else
-          
+
 
 
             <li class="nav-item">
               <a class="nav-link" href="{{ route('mycart')}}">@lang('My Cart') </a>
             </li>
-            
+
             <li class="nav-item">
               {!! Form::open(['route' => 'logout' , 'id' => 'logout-form']) !!}
                 <a class="nav-link" href="javascript:{}" onclick="document.getElementById('logout-form').submit();" >@lang('Logout') </a>
-                
-              {!! Form::close() !!}    
+
+              {!! Form::close() !!}
             </li>
             @endif
             <li class="nav-item">
               <a class="nav-link" href="#">@lang('Help')</a>
             </li>
 
-           
+
           </ul>
         </div>
       </nav>
@@ -46,19 +46,34 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
             <li class="nav-item {{ App::isLocale('en')  ? 'active':'' }}">
-              <a class="nav-link" href="/lang/en">English</a>
+              <a class="nav-link" href="/lang/en">
+                <img src='/front-end/images/usa-flag.png' class='icon-flag'>
+                English
+              </a>
             </li>
             <li class="nav-item {{ App::isLocale('ar')  ? 'active':'' }}">
-              <a class="nav-link" href="/lang/ar">Arabic</a>
+              <a class="nav-link" href="/lang/ar">
+                <img src='/front-end/images/arabic-flag.png' class='icon-flag'>
+                Arabic
+              </a>
             </li>
             <li class="nav-item {{ App::isLocale('de')  ? 'active':'' }}">
-              <a class="nav-link" href="/lang/de">German</a>
+              <a class="nav-link" href="/lang/de">
+                <img src='/front-end/images/germany-flag.png' class='icon-flag'>
+                German
+              </a>
             </li>
             <li class="nav-item {{ App::isLocale('ku')  ? 'active':'' }}">
-              <a class="nav-link" href="/lang/ku">Kurdish</a>
+              <a class="nav-link" href="/lang/ku">
+                <img src='/front-end/images/kurdish-flag.png' class='icon-flag'>
+                Kurdish
+              </a>
             </li>
             <li class="nav-item {{ App::isLocale('tr')  ? 'active':'' }}">
-              <a class="nav-link" href="/lang/tr">Turkish</a>
+              <a class="nav-link" href="/lang/tr">
+                <img src='/front-end/images/turkey-flag.png' class='icon-flag'>
+                Turkish
+              </a>
             </li>
           </ul>
         </div>
