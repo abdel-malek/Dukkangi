d2b90354436698170898140ccd425b90615bc209
Moustafa22 <36955328+Moustafa22@users.noreply.github.com>
2018-06-05 17:23:48 +0300

    review csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -24,6 +24,7 @@ class VerifyCsrfToken extends BaseVerifier
       '/admin/coupon',
       '/changeqty',
       '/getreview',
-      '/admin-login'
+      '/admin-login',
+      '/review'
     ];
 }
